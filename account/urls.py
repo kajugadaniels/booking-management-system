4b8ea37f39bf15cb92a935dcd70b7513e4578a08
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 16:54:19 +0200

    Wire loginUser view to login URL in account.urls

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    path('login/', getLogin, name="getLogin"),
     path('register/', getRegister, name="getRegister"),
+    path('login/', getLogin, name="getLogin"),
     path('forget-password/', forgetPassword, name="forgetPassword"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
