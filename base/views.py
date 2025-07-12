436ac877f49507b172f2fb86affb839cf6ceb517
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 11:42:33 +0200

    Add home function

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from base.models import *
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def home(request):
+    settings = Setting.objects.first()
+
+    context = {
+        'settings': settings
+    }
+
+    return render (request, 'pages/index.html', context)
