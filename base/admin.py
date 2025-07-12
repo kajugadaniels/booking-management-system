04e16563f58f3306a36b1545e4859015762633e6
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 20:15:33 +0200

    Remove HotelRoom, RoomImage, Amenity, AmenityRoom and RoomReview model from base app admin to hotel app admin

--- base/admin.py
+++ base/admin.py
@@ -14,31 +14,3 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')
     search_fields = ('first_name', 'last_name', 'email', 'phone')
     readonly_fields = ('created_at',)
-
-class RoomImageInline(admin.TabularInline):
-    model = RoomImage
-    extra = 1
-    readonly_fields = ['uploaded_at']
-
-class RoomAmenityInline(admin.TabularInline):
-    model = RoomAmenity
-    extra = 1
-
-class RoomReviewInline(admin.TabularInline):
-    model = RoomReview
-    extra = 0
-    readonly_fields = ['user', 'rating', 'title', 'review', 'created_at']
-    can_delete = False
-
-@admin.register(HotelRoom)
-class HotelRoomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'hotel', 'room_type', 'occupancy', 'price_per_night', 'is_available')
-    list_filter = ('is_available', 'hotel', 'room_type', 'occupancy')
-    search_fields = ('name', 'hotel__name', 'room_type')
-    list_per_page = 10
-    inlines = [RoomImageInline, RoomAmenityInline, RoomReviewInline]
-
-@admin.register(Amenity)
-class AmenityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'icon', 'created_at')
-    search_fields = ('name',)
--- hotel/admin.py
+++ hotel/admin.py
@@ -37,3 +37,31 @@ class HotelAdmin(admin.ModelAdmin):
             'fields': ('created_at',),
         }),
     )
+
+class RoomImageInline(admin.TabularInline):
+    model = RoomImage
+    extra = 1
+    readonly_fields = ['uploaded_at']
+
+class RoomAmenityInline(admin.TabularInline):
+    model = RoomAmenity
+    extra = 1
+
+class RoomReviewInline(admin.TabularInline):
+    model = RoomReview
+    extra = 0
+    readonly_fields = ['user', 'rating', 'title', 'review', 'created_at']
+    can_delete = False
+
+@admin.register(HotelRoom)
+class HotelRoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hotel', 'room_type', 'occupancy', 'price_per_night', 'is_available')
+    list_filter = ('is_available', 'hotel', 'room_type', 'occupancy')
+    search_fields = ('name', 'hotel__name', 'room_type')
+    list_per_page = 10
+    inlines = [RoomImageInline, RoomAmenityInline, RoomReviewInline]
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon', 'created_at')
+    search_fields = ('name',)
