b60d3c2dc5dcdd2d8d6a0ad5ba7c56bd8cc99efa
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 11:51:33 +0200

    Add plane app

---
+++ plane/urls.py
@@ -0,0 +1,10 @@
+from plane.views import *
+from django.conf import settings
+from django.urls import path, re_path
+from django.conf.urls.static import static
+
+app_name = 'plane'
+
+urlpatterns = [
+    # path('login/', getLogin, name="getLogin"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
