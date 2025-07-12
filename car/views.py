52e2dd51f0186a267a7250224d18dcd0548c5e00
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 11:52:20 +0200

    Add getCars funciton

--- car/views.py
+++ car/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from base.models import *
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def getCars(request):
+    settings = Setting.objects.first()
+
+    context = {
+        'settings': settings
+    }
+
+    return render (request, 'pages/cars/index.html', context)
