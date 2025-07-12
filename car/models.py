b21c29725ca56add05a47332a5f1ff663b904ba6
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 21:16:55 +0200

    Add CarType model to store types of cars

--- car/models.py
+++ car/models.py
@@ -1,3 +1,22 @@
+import os
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+def car_image_upload_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    timestamp = timezone.now().strftime('%Y%m%d%H%M%S')
+    return f"cars/car_{slugify(instance.car.name)}_{timestamp}{file_extension}"
+
+
+class CarType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Car Type"
+        verbose_name_plural = "Car Types"
