848c2f494501ff74940ddd2e89776ba9dbcdc20c
kajugadaniels <kajugadaniels@gmail.com>
2025-07-13 00:44:38 +0200

    Add RoomReviewForm for user room reviews

---
+++ hotel/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from hotel.models import *
+
+class RoomReviewForm(forms.ModelForm):
+    class Meta:
+        model = RoomReview
+        fields = ['rating', 'title', 'review']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Good Room', 'class': 'form-control'}),
+            'review': forms.Textarea(attrs={'placeholder': 'Write your review here...', 'class': 'form-control', 'rows': 4}),
+            'rating': forms.Select(choices=[(i, f'{i} Stars') for i in range(1, 6)], attrs={'class': 'form-control'})
+        }
