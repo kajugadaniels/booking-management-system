c96bca1c26e9bbe7fd59c0cfb93c9dde17b824cb
kajugadaniels <kajugadaniels@gmail.com>
2025-07-12 11:39:05 +0200

    Add login function

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from account.views import *
+from django.conf import settings
+from django.urls import path, re_path
+from django.conf.urls.static import static
+
+app_name = 'auth'
+
+urlpatterns = [
+    path('login/', getLogin, name="getLogin"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
