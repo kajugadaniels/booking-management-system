6b87b29f21a0f070ae0daab77f0be6752262f5c4
kajugadaniels <kajugadaniels@gmail.com>
2025-07-13 20:09:00 +0200

    Send plane booking request

--- plane/views.py
+++ plane/views.py
@@ -1,5 +1,6 @@
 from base.models import *
 from plane.forms import *
+from django.conf import settings
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
