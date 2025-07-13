d957aac266aecfd0de407b12b13e371cb1a02e41
kajugadaniels <kajugadaniels@gmail.com>
2025-07-13 17:58:51 +0200

    Add CarReviewForm function

---
+++ car/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from car.models import *
+from django.utils import timezone
+
+class CarReviewForm(forms.ModelForm):
+    class Meta:
+        model = CarReview
+        fields = ['rating', 'title', 'review']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Good Room', 'class': 'form-control'}),
+            'review': forms.Textarea(attrs={'placeholder': 'Write your review here...', 'rows': 4}),
+            'rating': forms.Select(choices=[(i, f'{i} Stars') for i in range(1, 6)], attrs={'class': 'form-control'})
+        }
