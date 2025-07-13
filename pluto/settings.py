756d939c09100be94aaed21f8f1e9e56cdc6857a
kajugadaniels <kajugadaniels@gmail.com>
2025-07-14 01:46:03 +0200

    New changes

--- pluto/settings.py
+++ pluto/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'pluto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES =  {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("cometboo_pluto"),
+        'USER': os.getenv("cometboo_pluto"),
+        'PASSWORD': os.getenv("WyT%.ZGGme3W4ucd"),
+        'HOST': os.getenv("localhost"),
+        'PORT': os.getenv("3306"),
     }
 }
 
