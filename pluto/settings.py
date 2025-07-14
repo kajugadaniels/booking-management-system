ba9621840b38bcfb6ce3db2a952099c104ea37f6
kajugadaniels <kajugadaniels@gmail.com>
2025-07-14 02:04:15 +0200

    Add email smtp

--- pluto/settings.py
+++ pluto/settings.py
@@ -153,11 +153,11 @@ AUTH_USER_MODEL = 'account.User'
 
 # Email Backend Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST = os.getenv('EMAIL_HOST', "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "mucyora@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "senbqvdwsxihgqgh")
 
 JAZZMIN_SETTINGS = {
     "site_title": "Pluto",
