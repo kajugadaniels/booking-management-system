d2e8d707cf83ac45a203aa73eafbc68998014e79
kajugadaniels <kajugadaniels@gmail.com>
2025-07-14 02:12:49 +0200

    Add passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,12 @@
-import sys
 import os
+import sys
 
-# Add your project directory to the sys.path
+# Add the project directory to sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Set the Django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pluto.settings'
 
-# Get the WSGI application
+# Load the WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
