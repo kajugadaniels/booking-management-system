a97f8b5c6ea1ef45318370c2632e7c050f79235e
kajugadaniels <kajugadaniels@gmail.com>
2025-07-16 20:32:48 +0200

    Add .env in passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,18 @@
 import os
 import sys
+from pathlib import Path
 
-# Add the project root directory to sys.path
+# Add project root to sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Set the Django settings module
+# Load .env
+from dotenv import load_dotenv
+env_path = Path(__file__).resolve().parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pluto.settings')
 
-# Get the WSGI application for the Django project
+# Load WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
