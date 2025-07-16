f6eb20564fcccc6b6cc825265ca84b71d9cf4340
kajugadaniels <kajugadaniels@gmail.com>
2025-07-16 19:38:00 +0200

    Add route for car booking payment: /car/<invoice_number>/pay/

--- payment/urls.py
+++ payment/urls.py
@@ -7,4 +7,5 @@ app_name = 'payment'
 
 urlpatterns = [
     path('room/<str:invoice_number>/pay/', payRoomBooking, name='payRoomBooking'),
+    path('car/<str:invoice_number>/pay/', payCarBooking, name='payCarBooking'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
